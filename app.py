672181fcdb5e183e0da5aa3e4df45ad3276dbd17
Chaojie Yan <cyan-ext@appannie.com>
2018-10-24 20:37:56 +0800

    add WTForm

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 # Copyright (c) 2018 Usharerose. All rights reserved.
 # -*- coding: utf-8 -*-
-from flask import Flask, make_response, render_template
+from datetime import datetime
+
+from flask import Flask, make_response, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    response = make_response(render_template('index.html'))
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    response = make_response(render_template('index.html', current_time=datetime.utcnow(), form=form, name=name))
     response.set_cookie('answer', '42')
     return response
 
@@ -21,6 +40,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
