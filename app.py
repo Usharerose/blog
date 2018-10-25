a8e688fb78c342620c194c2a4dac058a7e9bcac7
Chaojie Yan <cyan-ext@appannie.com>
2018-10-25 16:05:13 +0800

    add email

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 # Copyright (c) 2018 Usharerose. All rights reserved.
 # -*- coding: utf-8 -*-
 from datetime import datetime
+import os
 
 from flask import Flask, make_response, render_template, session, redirect, url_for, flash
+from flask_mail import Mail, Message
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -23,19 +25,33 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <Flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-
-db = SQLAlchemy(app)
+mail = Mail(app)
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,6 +64,8 @@ def index():
             db_session.add(user)
             db_session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         old_name = session.get('name')
